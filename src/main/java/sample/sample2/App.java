d52d748d43b81ae0a6558562b49165bfec60dc73
sagar <sagar.dalvi@biz4solutions.com>
2022-05-05 17:58:36 +0530

    second commit

--- src/main/java/sample/sample2/App.java
+++ src/main/java/sample/sample2/App.java
@@ -3,5 +3,7 @@ package sample.sample2;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    
+    System.out.println("hello prashant");
   }
 }
